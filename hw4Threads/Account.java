e3363b43e62850ab294c8db35da340043384e05f
FuTu <non>
2018-12-07 12:26:27 -0800

    Part A original

---
+++ hw4Threads/Account.java
@@ -0,0 +1,24 @@
+// Account.java
+
+/*
+ Simple, thread-safe Account class encapsulates
+ a balance and a transaction count.
+*/
+public class Account {
+    private int id;
+	private int balance;
+	private int transactions;
+	
+	// It may work out to be handy for the account to
+	// have a pointer to its Bank.
+	// (a suggestion, not a requirement)
+	private Bank bank;  
+	
+	public Account(Bank bank, int id, int balance) {
+		this.bank = bank;
+		this.id = id;
+		this.balance = balance;
+		transactions = 0;
+	}
+	
+}
---
+++ hw4Threads/Bank.java
@@ -0,0 +1,80 @@
+// Bank.java
+
+/*
+ Creates a bunch of accounts and uses threads
+ to post transactions to the accounts concurrently.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Bank {
+	public static final int ACCOUNTS = 20;	 // number of accounts
+	
+
+	
+	/*
+	 Reads transaction data (from/to/amt) from a file for processing.
+	 (provided code)
+	 */
+	public void readFile(String file) {
+			try {
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			
+			// Use stream tokenizer to get successive words from file
+			StreamTokenizer tokenizer = new StreamTokenizer(reader);
+			
+			while (true) {
+				int read = tokenizer.nextToken();
+				if (read == StreamTokenizer.TT_EOF) break;  // detect EOF
+				int from = (int)tokenizer.nval;
+				
+				tokenizer.nextToken();
+				int to = (int)tokenizer.nval;
+				
+				tokenizer.nextToken();
+				int amount = (int)tokenizer.nval;
+				
+				// Use the from/to/amount
+				
+				// YOUR CODE HERE
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	/*
+	 Processes one file of transaction data
+	 -fork off workers
+	 -read file into the buffer
+	 -wait for the workers to finish
+	*/
+	public void processFile(String file, int numWorkers) {
+	}
+
+	
+	
+	/*
+	 Looks at commandline args and calls Bank processing.
+	*/
+	public static void main(String[] args) {
+		// deal with command-lines args
+		if (args.length == 0) {
+			System.out.println("Args: transaction-file [num-workers [limit]]");
+			System.exit(1);
+		}
+		
+		String file = args[0];
+		
+		int numWorkers = 1;
+		if (args.length >= 2) {
+			numWorkers = Integer.parseInt(args[1]);
+		}
+		
+		// YOUR CODE HERE
+	}
+}
+
